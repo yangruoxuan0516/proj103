91310e61510e2c9e7f1ad570894d590184381a70
yangruoxuan0516 <yangruoxuan0516@126.com>
2023-11-25 18:17:46 +0100

    add auto code

--- app.py
+++ app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, Response
 import cv2
 import atexit
 import motor.motor as motor
-import aruco.detect_aruco as aruco
+import aruco.detect_aruco as detect
+import reach_aruco as reach
 
 app = Flask(__name__)
 Motor = motor.MotorDriver()
 cap = cv2.VideoCapture(0) 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-
+    
 @app.route('/')
-def index():
-    return render_template('index.html')
-
+def manual():
+    return render_template('manual.html')
 
 def video_result():
     while True:
@@ -21,14 +21,14 @@ def video_result():
         if not success:
             break
         else:
-            frame = aruco.detect_aruco(frame)
+            frame = detect.detect_aruco(frame)
             _, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             # This step is necessary because the Flask Response object expects
             # a byte-like object for streaming.
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
+            
 @app.route('/video')
 def video():
     return Response(video_result(), mimetype='multipart/x-mixed-replace; boundary=frame')
@@ -38,35 +38,43 @@ def video():
 def forward():
     motor.forward(Motor,100)
     going="going forward"
-    return render_template('index.html',going=going)  
+    return render_template('manual.html',going=going)  
 
 @app.route("/backward")
 def backward():
     motor.backward(Motor,100)
     going = "going backward"
-    return render_template('index.html', going=going)
+    return render_template('manual.html', going=going)
 
 @app.route("/left")
 def left():
     motor.left(Motor,100)
     going = "going left"
-    return render_template('index.html', going=going)
+    return render_template('manual.html', going=going)
 
 @app.route("/right")
 def right():
     motor.right(Motor,100)
     going = "going right"
-    return render_template('index.html', going=going)
+    return render_template('manual.html', going=going)
 
 @app.route("/stop")
 def stop():
     motor.stop(Motor)
     going = "stopped"
-    return render_template('index.html', going=going)
+    return render_template('manual.html', going=going)
 
+@app.route("/auto",methods=['GET'])
+def auto():
+    _, image = cap.read()
+    reach.reach_aruco(image)
+    return render_template('auto.html')
 
 atexit.register(stop)
 
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
+
+
+
+            
---
+++ reach_aruco.py
@@ -0,0 +1,83 @@
+import cv2
+import motor.motor as motor
+import aruco.detect_aruco as detect
+import time
+
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+arucoParams = cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(arucoDict, arucoParams)
+Motor = motor.MotorDriver()
+
+def reach_aruco(image):
+    found = False
+    left = True # need to turn left
+    while True:
+        [direction,distance] = detect_aruco(image)
+        if direction:
+            found = True            
+            if direction == 1:
+                turn_left_a_little()
+                left = False
+            elif direction == 3:
+                turn_right_a_little()
+                left = True
+            elif direction == 2 and distance <= 25:
+                go_forward_a_little()
+            else:
+                break
+        elif found:
+            if left:
+                turn_left_a_little()
+                left = False
+            else:
+                turn_right_a_little()
+                left = True
+        else:
+            turn_left_a_little()
+
+
+def detect_aruco(image):
+    (corners, ids, _) = detector.detectMarkers(image)
+    # Each markerCorner is represented by a list of four (x, y)-coordinates
+    # (top-left, top-right, bottom-right, and bottom-left)
+    if len(corners) > 0:
+        ids = ids.flatten()
+	    # loop over the detected ArUCo corners
+        for (markerCorner, markerID) in zip(corners, ids):
+		    # extract the marker corners (which are always returned in
+		    # top-left, top-right, bottom-right, and bottom-left order)
+            corners = markerCorner.reshape((4, 2))
+            (topLeft, topRight, bottomRight, bottomLeft) = corners
+		    # convert each of the (x, y)-coordinate pairs to integers
+            topRight = (int(topRight[0]), int(topRight[1]))
+            bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+            bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+            topLeft = (int(topLeft[0]), int(topLeft[1]))
+            cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+            distance = int(21000/(bottomRight[1]-topRight[1]+bottomLeft[1]-topLeft[1]))
+            _ , width, _ = image.shape
+            if cX < width*0.4:
+                direction = 1
+            elif cX > width*0.6:
+                direction = 3
+            else:
+                direction = 2
+            return [direction,distance]
+    return [0,None]
+    
+def turn_left_a_little():
+    motor.left(Motor,40)
+    time.sleep(0.2)
+
+def turn_right_a_little():
+    motor.right(Motor,40)
+    time.sleep(0.2)
+
+def go_forward_a_little():
+     motor.forward(Motor,40)
+     time.sleep(0.2)
+
+
+    
+
+
